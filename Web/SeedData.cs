516e4fc21f7a504cb16826919baf7ba7a658eee6
hrymarz1234 <126766484+hrymarz1234@users.noreply.github.com>
2024-03-18 17:39:00 +0100

    Dodaj pliki projektów.

---
+++ Web/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using ApplicationCore.Interfaces.Repository;
+using BackendLab01;
+
+namespace Infrastructure.Memory;
+public static class SeedData
+{
+    public static void Seed(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var provider = scope.ServiceProvider;
+            var quizRepo = provider.GetService<IGenericRepository<Quiz, int>>();
+            var quizItemRepo = provider.GetService<IGenericRepository<QuizItem, int>>();
+
+            QuizItem m1= new QuizItem (1, "3+1", new List<string> { "3", "1", "5", "7", "2" }, "4");
+            QuizItem m2 = new QuizItem(2, "2*7", new List<string> { "11", "21", "43", "54", "32" }, "14");
+            QuizItem m3 = new QuizItem(3, "10-7", new List<string> { "2", "1", "5", "7", "9" }, "3");
+            QuizItem m4 = new QuizItem(4, "16/4", new List<string> { "3", "1", "5", "7", "2" }, "4");
+
+            QuizItem g1 = new QuizItem(1, "Gdzie leży Kanada?", new List<string> { "Ameryka Płd", "Europa", "Azja", "Afryka", "Australia" }, "Ameryka Płn");
+            QuizItem g2 = new QuizItem(2, "Gdzie leży Wietnam?", new List<string> { "Europa", "Ameryka Płd", "Afryka", "Ameryka Płn", "Australia" }, "Azja");
+            QuizItem g3 = new QuizItem(3, "Gdzie leży Libia?", new List<string> { "Ameryka Płn", "Europa", "Australia", "Azja", "Ameryka Płd" }, "Afryka");
+
+        }
+    }
+}
